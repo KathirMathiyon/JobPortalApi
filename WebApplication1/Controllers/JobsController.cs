40f1d094902e9b4567a61c8f0700474b11c8d052
Kathirmathiyon Kandasamy <KA40084672@wipro.com>
2026-06-16 22:38:00 +0530

    GetAllJobs controller

---
+++ WebApplication1/Controllers/JobsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/jobs")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        public IActionResult GetAllJobs()
+        {
+            var jobs = new[]
+            {
+                new { Id = 1, Title = "Software Engineer", Company = "Google", Location = "Bangalore"},
+                new { Id = 1, Title = "Backend Engineer", Company = "ZOHO", Location = "Chennai"},
+                new { Id = 1, Title = "Full Stack Engineer", Company = "Freshworks", Location = "Chennai"},
+            };
+
+            return Ok(jobs);
+        }
+    }
+}
