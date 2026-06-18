223855c13923c07fa4b8571d403187a2ab622f64
Kathirmathiyon Kandasamy <KA40084672@wipro.com>
2026-06-18 11:50:23 +0530

    CRUD OPERATIONS PUT AND DELETE

--- WebApplication1/Controllers/JobsController.cs
+++ WebApplication1/Controllers/JobsController.cs
@@ -41,5 +41,38 @@ namespace WebApplication1.Controllers
             await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetJobsById), new { id = job.Id }, job);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateJob(int id, Job updatedJob)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+
+            if (job == null)
+            {
+                return NotFound($"The Job {id} is not found");
+            }
+
+            job.Title = updatedJob.Title;
+            job.Company = updatedJob.Company;
+            job.Location = updatedJob.Location;
+            job.PostedDate = updatedJob.PostedDate;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(job);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound($"The Job {id} is not found");
+            }
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+            return Ok($"The job {id} has been deleted.");
+        }
     }
 }
